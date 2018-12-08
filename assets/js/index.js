b7020fd813bcf31f99a200d47be8398b07b5fdab
Alger Makiputin <algerzxc@gmail.com>
2018-12-08 21:33:52 +0800

    dev

--- assets/js/index.js
+++ assets/js/index.js
@@ -254,39 +254,39 @@ $(document).ready(function() {
 		}
 	})
 
- 	if ($(location).attr('href').split('/')[4] == "sales") {
- 		var ctx = document.getElementById("myChart");
-		var myChart = new Chart(ctx, {
-		    type: 'line',
-		    data: {
-		        labels: labels,
-		        datasets: [{
-		            label: 'Sales for the Last 7 Days',
-		            data: totalSales,
-		            backgroundColor: [
-		                '#337ab7',
-		            ],
-		            strokeColor: [
-		                '#337ab7',
-		            ],
-		            borderWidth: 1
-		        }]
-		    },
-		    options: {
-		        scales: {
-		            yAxes: [{
-		                ticks: {
-		                    beginAtZero:true,
-		                    callback : function(value, index, values) {
-		                    	return '₱' + (value);
+  
+	var ctx = document.getElementById("myChart");
+	var myChart = new Chart(ctx, {
+	    type: 'line',
+	    data: {
+	        labels: labels,
+	        datasets: [{
+	            label: 'Sales for the Last 7 Days',
+	            data: totalSales,
+	            backgroundColor: [
+	                '#337ab7',
+	            ],
+	            strokeColor: [
+	                '#337ab7',
+	            ],
+	            borderWidth: 1
+	        }]
+	    },
+	    options: {
+	        scales: {
+	            yAxes: [{
+	                ticks: {
+	                    beginAtZero:true,
+	                    callback : function(value, index, values) {
+	                    	return '₱' + (value);
 
-		                    }
-		                }
-		            }]
-		        }
-		    }
-		}); 
- 	}
+	                    }
+	                }
+	            }]
+	        }
+	    }
+	}); 
+  
 	
 })
 
