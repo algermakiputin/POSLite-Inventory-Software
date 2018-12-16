be209b53ae7851f2c1d123e8747ef79cb48ab0c5
Alger Makiputin <algerzxc@gmail.com>
2018-12-16 08:37:32 +0800

    push

--- application/views/items/stockout.php
+++ application/views/items/stockout.php
@@ -15,7 +15,7 @@
 				<div class="row">
 					<div class="col-md-12">
 						<div class="form-group">
-							<button class="btn btn-primary" id="mail" disabled="disabled"> data-loading-text="<i class='fa fa-spinner fa-spin'></i> Sending Email...">Mail Order</button>
+							<button class="btn btn-primary" id="mail" disabled="disabled" data-loading-text="<i class='fa fa-spinner fa-spin'></i> Sending Email...">Mail Order</button>
 						</div>
 
 						<div id="message" style="display: none;">
