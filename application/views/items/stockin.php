82e1fd5838063d405051e33a1ff19a83262f89aa
alger makiputin <algerzxc@gmail.com>
2019-01-10 20:30:47 +0800

    a.

--- application/views/items/stockin.php
+++ application/views/items/stockin.php
@@ -22,7 +22,7 @@
 						<div class="form-group"> 
 							<label>Add Stocks:</label>
 							<input type="hidden" name="item_name" value="<?php echo $item_info->name; ?>">
-							<input type="number" max="500" name="stocks" class="form-control" placeholder="Enter Stocks To Add">
+							<input type="number" <?php if(SITE_LIVE) echo 'max="500"'; ?> name="stocks" class="form-control" placeholder="Enter Stocks To Add">
 							<input type="hidden" name="item_id" value="<?php echo $item_id; ?>">
 						</div>
 						 
