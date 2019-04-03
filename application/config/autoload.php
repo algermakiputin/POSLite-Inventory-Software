75e526987611fbdadbe13e30b1980d582bc7bb5e
alger makiputin <algerzxc@gmail.com>
2019-04-03 16:52:06 +0800

    a

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url','form','date','functions');
+$autoload['helper'] = array('url','form','date','functions','security');
 
 /*
 | -------------------------------------------------------------------
