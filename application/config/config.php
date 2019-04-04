89c23fbd31701a55f18b33f758ec39b402c90663
alger makiputin <algerzxc@gmail.com>
2019-04-04 09:13:01 +0800

    a

--- application/config/config.php
+++ application/config/config.php
@@ -458,7 +458,12 @@ $config['csrf_token_name'] = 'csrf_test_name';
 $config['csrf_cookie_name'] = 'csrf_cookie_name';
 $config['csrf_expire'] = 7200;
 $config['csrf_regenerate'] = TRUE;
-$config['csrf_exclude_uris'] = array('items/data','sales/report','SalesController/insert');
+$config['csrf_exclude_uris'] = array(
+									'items/data',
+									'sales/report',
+									'SalesController/insert',
+									'sales/graph-filter'
+								);
 
 /*
 |--------------------------------------------------------------------------
