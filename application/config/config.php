8f96f7396c240f5a90f37ae9fdaa8316eb5918f7
alger makiputin <algerzxc@gmail.com>
2019-05-02 07:53:09 +0800

    Config Time Zone Asia/Manila

--- application/config/config.php
+++ application/config/config.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
+date_default_timezone_set('Asia/Manila');
 /*
 |--------------------------------------------------------------------------
 | Base Site URL
