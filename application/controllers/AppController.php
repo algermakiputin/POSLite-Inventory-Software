560dd7171c7c727ae9ad54e1104f1e665537020f
alger makiputin <algerzxc@gmail.com>
2019-07-06 08:56:18 +0800

    a

--- application/controllers/AppController.php
+++ application/controllers/AppController.php
@@ -6,5 +6,26 @@ class AppController extends CI_Controller {
        parent::__construct();
     }
 
+    public function userAccess($page) {
+
+		$data = [
+			'new' => ['Admin', 'Cashier'],
+			'edit' => ['Admin', 'Cashier', 'Clerk'],
+			'view' => ['Admin', 'Cashier', 'Clerk'],
+			'customers' => ['Admin', 'Cashier', 'Clerk'],
+			'expenses' => ['Admin','Cashier'],
+			'user' => ['Admin'],
+			'user_register' => ['Admin'],
+			'new_delivery' => ['Admin'],
+			'delivery' => ['Admin'],
+			'expenses_new' => ['Admin']
+		];
+
+		if (!in_array($this->session->userdata('account_type'), $data[$page])) {
+			return redirect('/');
+		}
+
+	}
+
 }
  
