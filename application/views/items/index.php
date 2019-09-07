4098dc7c01766751be666b36cd50ee5348477767
alger makiputin <algerzxc@gmail.com>
2019-09-08 07:53:31 +0800

    product default datatable lenght change to 10

--- application/views/items/index.php
+++ application/views/items/index.php
@@ -1,7 +1,7 @@
 <div class="row">
 	<div class="col-lg-12">
 		<h1 class="page-header">Products
-            <span class="pull-right">Inventory Value: ₱<span id="total"><?php echo $total; ?></span></span>
+            <!-- <span class="pull-right">Inventory Value: ₱<span id="total"><?php echo $total; ?></span></span> -->
         </h1> 
     </div>
     <div class="col-md-12">
