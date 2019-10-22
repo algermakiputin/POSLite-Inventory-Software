7dac9e0545c78e40af5f3bf9024cc2234c1add2c
alger makiputin <algerzxc@gmail.com>
2019-10-22 17:14:01 +0800

    low stock & stockout notification

--- application/views/master.php
+++ application/views/master.php
@@ -32,11 +32,7 @@
             <ul class="nav navbar-top-links navbar-right">
                
                 <!-- /.dropdown -->
-                <?php if ($count = noStocks()->num_rows()) : ?>
-                <li>
-                    <a href="<?php echo base_url('out-of-stocks') ?>">Out of Stock <span style="margin-top: -10px;" class="badge badge-error"><?php echo $count ?></span></a>
-                </li>
-                <?php endif; ?>
+         
                 <li >
                     <a href="#" onclick="event.preventDefault(); javascript:introJs().start()">Start Demo</a>
                 </li>
@@ -77,27 +73,123 @@
         <p style="color: #333">Developed By: <a href="https://algermakiputin.com">Alger Makiputin</a></p>
     </footer>
 
+    
 </body>
+
+ 
+<script type="text/javascript">
+    var no_stocks = <?= json_encode(noStocks()); ?>;
+    var low_stocks = <?= json_encode(low_stocks()); ?>;
+
+    $(document).ready(function() {
+
+        var disable_stockout_notification = getCookie("disable_stockout_popup");
+        var disable_lowstock_notification = getCookie("disable_lowstock_popup");
+      
+        if (Object.keys(no_stocks).length && disable_stockout_notification != "1") { 
+            setTimeout(out_of_stock_popup, 10000);
+        }
+
+        if (Object.keys(low_stocks).length && disable_lowstock_notification != "1") { 
+            setTimeout(low_stock_popup, 20000);
+        }
+
+        //alert(getCookie("disablepopup"));
+        function set_disable_notification_cookie(name) {
+            var now = new Date();
+            now.setTime(now.getTime() + 1 * 3600 * 1000);
+            document.cookie = name+"=1; expires=" + now.toUTCString() + "; path=/";
+        }
+
+        function getCookie(cname) {
+            var name = cname + "=";
+            var ca = document.cookie.split(';');
+            for(var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') {
+              c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+              return c.substring(name.length, c.length);
+            }
+          }
+          return "";
+        }
+
+        function out_of_stock_popup() {
+            var tbody = "";
+            $.each(no_stocks, function(key, value) {
+
+                tbody += "<tr>"+
+                    "<td>"+value.name+"</td>" + 
+                    "<td>"+value.description+"</td>" + 
+                    "<td>"+value.quantity+"</td>" + 
+                "</tr>";
+            });
+
+            $.confirm({
+                icon: 'fa fa-warning text-danger',
+                title: 'Run out of stocks',
+                content: "<table class='table table-bordered table-striped'><thead><tr><td>Product</td><td>Description</td><td>Out of Stocks</td></tr></thead><tbody>"+tbody+"</tbody></table>",
+                columnClass: 'col-md-6 col-md-offset-3',
+                type: 'red',
+                buttons: {
+                    ok: {
+                        text: "OK",
+                        btnClass: "btn-default",
+                        action: function() {},
+                    },
+
+                    disable: {
+                        text: "Do now show this for 1HR",
+                        btnClass: "btn-danger",
+                        action: function() {
+                            set_disable_notification_cookie("disable_stockout_popup");
+                            alert("Low stock notification will be disabled for 1 hour.")
+                        }
+                    }
+                }
+            });
+        }
+
+        function low_stock_popup() {
+            var tbody = "";
+            $.each(low_stocks, function(key, value) {
+
+                tbody += "<tr>"+
+                    "<td>"+value.name+"</td>" + 
+                    "<td>"+value.description+"</td>" + 
+                    "<td>"+value.quantity+"</td>" + 
+                "</tr>";
+            });
+
+            $.confirm({
+                icon: 'fa fa-warning text-warning',
+                title: 'Low stocks',
+                content: "<table class='table table-bordered table-striped'><thead><tr><td>Product</td><td>Description</td><td>Low Stock</td></tr></thead><tbody>"+tbody+"</tbody></table>",
+                columnClass: 'col-md-6 col-md-offset-3',
+                type: 'orange',
+                buttons: {
+                    ok: {
+                        text: "OK",
+                        btnClass: "btn-default",
+                        action: function() {},
+                    },
+
+                    disable: {
+                        text: "Do now show this for 1HR",
+                        btnClass: "btn-warning",
+                        action: function() {
+                            set_disable_notification_cookie("disable_lowstock_popup");
+                            alert("Stock out notification will be disabled for 1 hour.")
+                        }
+                    }
+                }
+            });
+        }
+    });
+</script>
 
 </html>
 
-
-<!-- <script type="text/javascript">
-    var data = {};
-    data['<?php echo $this->security->get_csrf_token_name(); ?>'] = '<?php echo $this->security->get_csrf_hash(); ?>'; 
-    data['test'] = 'test';
-    data['test2'] = 'test3';
-    $.ajax({
-        type : 'POST',
-        url : 'http://localhost/license/index.php/LicenseController/validateLicense',
-        data : {
-            id : '',
-            test : ''
-        },
-        success : function(data) {
-            alert(data);
-        }
-
-    }) 
-    
-</script> -->
+ 
