4a6ba2802a34cdf21a94b6fbbf34de8fb732b0a4
alger makiputin <algerzxc@gmail.com>
2019-10-25 12:28:28 +0800

    add webpage icon

--- application/views/master.php
+++ application/views/master.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-
+    <link rel="shortcut icon" type="image/x-icon" href="<?php echo base_url('assets/logo/poslite.png') ?>" />
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
