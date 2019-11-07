929f5e899cd1dfd9890ad466a1d6fed399672934
alger makiputin <algerzxc@gmail.com>
2019-11-07 20:51:35 +0800

    System generated date time

--- application/models/HistoryModel.php
+++ application/models/HistoryModel.php
@@ -4,7 +4,8 @@ class HistoryModel extends CI_Model {
 	public function insert($action) {
 		$data = array(
 				'user_id' => $this->session->userdata('id'),
-				'action' => $action
+				'action' => $action,
+				'date' => get_date_time(),
 			);
 		$this->db->insert('history', $data);
 	}
