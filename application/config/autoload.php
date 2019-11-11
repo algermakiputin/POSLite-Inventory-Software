5b971860201c163fa4a5ed4238c8ffe7abda8a66
alger makiputin <algerzxc@gmail.com>
2019-11-11 17:26:49 +0800

    add env

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('table','session','encryption','form_validation','database');
+$autoload['libraries'] = array('table','session','encryption','form_validation','database','env');
 
 /*
 | -------------------------------------------------------------------
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url','form','date','functions','security');
+$autoload['helper'] = array('url','form','date','functions','security','general');
 
 /*
 | -------------------------------------------------------------------
