00cd61a8cb621a9335f526f548cc026eff016cac
Alger Makiputin <algerzxc@gmail.com>
2019-12-02 20:14:08 +0800

    Revert "smaller screens support"
    This reverts commit 76d9565abb96af58097046819a21fc477ff9eebd.

--- application/views/expenses/index.php
+++ application/views/expenses/index.php
@@ -11,7 +11,7 @@
     </div>
 </div>
 <div class="row">
-    <div class="col-md-8" style="margin-bottom: 10px;">
+    <div class="col-md-6" style="margin-bottom: 10px;">
         <form class="form-inline" autocomplete="off">
             <div class="form-group">
                 <label>Filter Reports: &nbsp;</label> 
@@ -27,7 +27,7 @@
             </div>
         </form>
     </div>
-    <div class="col-md-4 text-right">
+    <div class="col-md-6 text-right">
         <div style="padding:10px;font-size: 16px;">
             <b>Total Expenses:</b> <span id="total"></span>
         </div>
