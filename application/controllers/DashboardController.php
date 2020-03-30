a0de5894e77b0dd8d0a9e795c681d39156388ffd
Alger Makiputin <algerzxc@gmail.com>
2020-03-30 15:58:39 +0800

    test error

--- application/controllers/DashboardController.php
+++ application/controllers/DashboardController.php
@@ -19,9 +19,9 @@ class DashboardController extends AppController {
 		$data['no_stocks'] = 1;//count(noStocks());
 		$data['average_sales_per_day'] = $this->average_sales_per_day();
 		$data['orders'] = $this->db->where('date_format(date_time, "%Y-%m-%d") =', date('Y-m-d'))->get('sales')->num_rows();
-		$data['sales'] = number_format($this->sales_model->get_sales(date('Y-m-d'))->total,2);
-		$data['expenses'] = 0//number_format( number_format($this->ExpensesModel->total, 2) );
-		$data['revenue'] = number_format( $this->sales_model->get_annual_sales(date('Y'))->total );
+		$data['sales'] = 0,//number_format($this->sales_model->get_sales(date('Y-m-d'))->total,2);
+		$data['expenses'] = 0;//number_format( number_format($this->ExpensesModel->total, 2) );
+		$data['revenue'] = 0;//number_format( $this->sales_model->get_annual_sales(date('Y'))->total );
 
 		$lastweek = date('Y-m-d', strtotime('-7 days'));
 		$today = date('Y-m-d');
