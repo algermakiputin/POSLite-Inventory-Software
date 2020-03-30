a92f81214d89788bf87e9c61624c45eaf958e15f
Alger Makiputin <algerzxc@gmail.com>
2020-03-30 16:15:49 +0800

    test error

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -1,4 +1,4 @@
-<div class="row">
+<!-- <div class="row">
 	<div class="col-lg-12">
 		<h1 class="page-header">Dashboard</h1> 
     </div> 
@@ -290,3 +290,4 @@ echo $this->session->flashdata('successMessage');
     })
 
 </script> 
+ -->
