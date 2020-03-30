1b05b92fe25b2a73c305b27859fca8518f93ad4c
Alger Makiputin <algerzxc@gmail.com>
2020-03-30 20:54:45 +0800

    add product shortcut key

--- application/views/pos/index.php
+++ application/views/pos/index.php
@@ -231,7 +231,7 @@
 		  <div class="modal-dialog" role="document">
 		    <div class="modal-content">
 		      <div class="modal-header">
-		        <h4 class="modal-title" id="product-name"><b>Product Name</b></h4>
+		        <h3 class="modal-title" id="product-name"><b>Product Name</b></h3>
 		       
 		      </div>
 		      <div class="modal-body"> 
@@ -262,7 +262,7 @@
 		      </div>
 		      <div class="modal-footer">
 		        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-		        <button type="button" class="btn btn-primary" id="add-product">ADD PRODUCT</button>
+		        <button type="button" class="btn btn-primary" id="add-product">ADD PRODUCT (Enter)</button>
 		      </div>
 		    </div>
 		  </div>
