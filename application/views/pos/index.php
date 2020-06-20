d13a7007462f1f88a05eb15d346cf9f1c2abf7af
Alger Makiputin <algerzxc@gmail.com>
2020-06-20 19:59:06 +0800

    select2 js

--- application/views/pos/index.php
+++ application/views/pos/index.php
@@ -266,6 +266,29 @@
 	</div>
 </div>
 
+<div class="modal " id="customer-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal-dialog modal-lg" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h4 class="modal-title">Select Customer</h4> 
+			</div>
+			<div class="modal-body">
+			 	<form>
+			 		<div class="form-group">
+			 			<label>Customer</label>
+			 			<select>
+			 				
+			 			</select>
+			 		</div>
+			 	</form>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button> 
+			</div>
+		</div>
+	</div>
+</div>
+
 <div class="modal " id="return-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
 	<div class="modal-dialog modal-lg" role="document">
 		<div class="modal-content">
